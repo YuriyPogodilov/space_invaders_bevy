2d4efed45368abf9921effd3797fd88cb5d98e29
Yuriy <aka4an@gmail.com>
2024-09-21 00:28:14 +0700

    Set game window title and resolution

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 mod player;
 mod bullet;
 
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowResolution};
 use bullet::BulletPlugin;
 use player::PlayerPlugin;
 
 fn main() {
+    let app_window = Some(Window {
+        title: "SpaceInvadersBevy".into(),
+        resolution: WindowResolution::new(1280.0, 720.0),
+        ..default()
+    });
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(
+            WindowPlugin {
+                primary_window: app_window,
+                ..default()
+            }
+        ))
         .add_plugins(PlayerPlugin)
         .add_plugins(BulletPlugin)
         .run();
